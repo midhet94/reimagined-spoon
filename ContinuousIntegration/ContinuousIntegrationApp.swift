3cc4655cffe5edb810394616d20e9708d9ffaee3
Midhet <midhet.sulemani@grove.co>
2026-04-28 13:23:49 +0530

    Initial iOS app

---
+++ ContinuousIntegration/ContinuousIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuousIntegrationApp.swift
+//  ContinuousIntegration
+//
+//  Created by Midhet Sulemani on 28/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ContinuousIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContinuousIntegrationTests/ContinuousIntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  ContinuousIntegrationTests.swift
+//  ContinuousIntegrationTests
+//
+//  Created by Midhet Sulemani on 28/04/26.
+//
+
+import Testing
+
+struct ContinuousIntegrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
